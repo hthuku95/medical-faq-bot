ca0174ab681b7e82506f62acc317b9c576c9fe50
Harry Thuku <harry@knabu.me>
2025-03-04 19:26:41 +0300

    Updated Medical FAQ Bot

--- src/utils/geminiUtils.js
+++ src/utils/geminiUtils.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyCf1OdM2hnmLeNuH4ABeJy6jFbolXWBQCQ"); 
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY); 
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export const answerMedicalQuestion = async (question) => {
